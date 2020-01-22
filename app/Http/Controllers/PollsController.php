e4b90be836ba23feb41b96d32347eecaa6dbb3d9
Viola Ceni <viola.ceni@cit.edu.al>
2020-01-22 11:28:46 +0100

    Renamed API Crud & added index function

--- app/Http/Controllers/PollsController.php
+++ app/Http/Controllers/PollsController.php
@@ -2,20 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Exports\PollsExport;
 use App\Http\Controllers\Controller;
-use App\Poll;
 use App\Http\Resources\Poll as PollResource;
+use App\Poll;
+use Excel;
 use Illuminate\Http\Request;
 use Validator;
+use yajra\Datatables\Datatables;
 
 class PollsController extends Controller
 {
-    public function index()
+    public function indexPoll()
     {
         return response()->json(Poll::get(), 200);
     }
 
-    public function show($id)
+    public function showPoll($id)
     {
         $poll = Poll::find($id);
         if (is_null($poll)) {
@@ -25,38 +28,111 @@ class PollsController extends Controller
         return response()->json($response, 200);
     }
 
-    public function store(Request $request)
+    public function storePoll(Request $request)
     {
         $rules = [
             'title' => 'required|max:255',
         ];
         $validator = Validator::make($request->all(), $rules);
-        if($validator->fails()){
+        if ($validator->fails()) {
             return response()->json($validator->errors(), 400);
         }
         $poll = Poll::create($request->all());
         return response()->json($poll, 201);
     }
 
-    public function update(Request $request, $id)
+    public function updatePoll(Request $request, $id)
     {
         $poll = Poll::find($id);
         $poll->update($request->all());
         return response()->json($poll, 200);
     }
 
-    public function delete(Request $request, Poll $poll)
+    public function deletePoll(Request $request, Poll $poll)
     {
         $poll->delete();
         return response()->json(null, 204);
     }
 
-    public function errors(){
+    public function errors()
+    {
         return response()->json(['messaage' => 'Payment is required'], 501);
     }
 
-    public function questions(Request $request, Poll $poll){
+    public function questions(Request $request, Poll $poll)
+    {
         $questions = $poll->questions;
         return response()->json($questions, 200);
+    }
+
+    public function export()
+    {
+
+        return Excel::download(new PollsExport, 'polls.csv');
+
+    }
+
+    public function index(Request $request)
+    {
+        if ($request->ajax()) {
+            $data = Poll::latest()->get();
+            return datatables()::of($data)
+                ->addIndexColumn()
+                ->addColumn('action', function ($row) {
+
+                    $btn = '<a href="javascript:void(0)" data-toggle="tooltip"  data-id="' . $row->id . '" data-original-title="Edit" class="edit btn btn-primary btn-sm editProduct">Edit</a>';
+
+                    $btn = $btn . ' <a href="javascript:void(0)" data-toggle="tooltip"  data-id="' . $row->id . '" data-original-title="Delete" class="btn btn-danger btn-sm deleteProduct">Delete</a>';
+
+                    return $btn;
+                })
+                ->rawColumns(['action'])
+                ->make(true);
+        }
+
+        return view('pollsAjax');
+    }
+
+    public function store(Request $request)
+    {
+        Poll::updateOrCreate(['id' => $request->product_id],
+            ['title' => $request->title]);
+
+        return response()->json(['success' => 'Poll saved successfully.']);
+    }
+
+    public function edit($id)
+    {
+        $poll = Poll::find($id);
+        return response()->json($poll);
+    }
+
+    public function destroy($id)
+    {
+        Poll::find($id)->delete();
+
+        return response()->json(['success' => 'Poll deleted successfully.']);
+    }
+
+    public function editableColumn(Request $request)
+    {
+        $poll = Poll::find($request->id);
+
+        if (is_null($poll)) {
+            return response([
+                'success' => false,
+                'message' => 'Poll not found',
+            ], 405);
+        }
+
+        $poll->{$request->column} = $request->value;
+
+        $poll->save();
+
+        return response([
+            'success' => true,
+            'message' => 'Poll updated',
+        ], 200);
+
     }
 }
