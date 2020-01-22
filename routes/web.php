64dadca34b6246a084f380ba5150d2ff6a1dc25f
Viola Ceni <viola.ceni@cit.edu.al>
2020-01-22 11:42:27 +0100

    Added routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('polls/export',['uses'=>'PollsController@export', 'as'=>'polls.export']);
+Route::resource('polls','PollsController');
+Route::post('/editableColumn', ['uses' => 'PollsController@editableColumn', 'as' => 'editableColumn']);
