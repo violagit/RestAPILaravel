e02a3d46f6cfe9d5408a04575f967cd5c9325a75
Viola Ceni <viola.ceni@cit.edu.al>
2020-01-23 17:42:03 +0100

    Created FileController & show method

---
+++ app/Http/Controllers/FilesController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FilesController extends Controller
+{
+    public function show()
+    {
+        return response()->download(storage_path('app\GraceHopper.pdf'), 'Amaizing Grace');
+    }
+}
