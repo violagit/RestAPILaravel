2d4fec11bc0bb7e62c373b9fc7b0dd1fbdf3608d
Viola Ceni <viola.ceni@cit.edu.al>
2020-01-24 16:15:41 +0100

    Added Logging middleware

---
+++ app/Http/Middleware/Logging.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Log; 
+class Logging
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        Log::debug($request->method());
+        return $next($request);
+    }
+}
