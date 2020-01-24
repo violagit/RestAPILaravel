45e8f4130e629efa511074b60a25a2292b60cd76
Viola Ceni <viola.ceni@cit.edu.al>
2020-01-24 11:29:02 +0100

    Added route for create file method

--- routes/api.php
+++ routes/api.php
@@ -25,3 +25,4 @@ Route::any('errors','PollsController@errors');
 Route::apiResource('questions', 'QuestionController');
 Route::get('polls/{poll}/questions','PollsController@questions');
 Route::get('files/get','FilesController@show');
+Route::post('files/create', 'FilesController@create');
